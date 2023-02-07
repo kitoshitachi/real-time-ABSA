2c15e6ac2fb4742acb39583d7b311b9d734fd281
kitoshitachi <19521204@gm.uit.edu.vn>
2023-02-07 18:41:25 +0700

    crawl good uwu

--- scraping.py
+++ scraping.py
@@ -1,34 +1,12 @@
 import json
 import re
-
+import sys
+from langdetect import detect
 import pandas as pd
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
 session = HTMLSession()
-
-
-def get_hotels(offset: int):
-    url = "https://www.tripadvisor.com.vn/Restaurants-g293921-Vietnam.html"
-
-    schema_json = f"offset={offset}"
-    #
-    headers = {
-        "accept": "text/html, */*",
-        "accept-language": "vi",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "x-requested-with": "XMLHttpRequest"
-    }
-
-    r = session.post(url, headers=headers, data=schema_json)
-
-    soup = BeautifulSoup(r.text, 'lxml')
-    result = soup.select_one('div[data-hotels-data]')['data-hotels-data']
-
-    contents_json = json.loads(result)
-    df = pd.json_normalize(contents_json['hotels'])[['id', 'name', 'numReviews']]
-    return df.drop_duplicates().dropna()[df['numReviews'] != 0]
-
 
 def get_reviews(location_id: int):
     schema_json = [{
@@ -79,36 +57,36 @@ def get_restaurants(city_id: int, offset: int):
     r = session.get(
         f"https://www.tripadvisor.com.vn/RestaurantSearch?Action=PAGE&ajax=1&availSearchEnabled=false&sortOrder=popularity&geo={city_id}&itags=10591&o=a{offset}")
     soup = BeautifulSoup(r.text, 'lxml')
-    result = soup.select_one('#component_2')['data-component-props']
-
-    items = [item for item in soup.select('div[data-test*=list_item]') if item.attrs["data-test"] == "SL_list_item"]
+    items = [item for item in soup.select('div[data-test*=list_item]') if item.attrs["data-test"] != "SL_list_item"]
     names = [re.sub('\d+\.\s+', '', item.select("a")[1].text, 1) for item in items]
     ids = [re.search('-d(\d+)', item.select("a")[1].attrs["href"])[1] for item in items]
-    df = pd.DataFrame({"Id": ids, "Name": names})
+    df = pd.DataFrame({"id": ids, "Name": names})
     return df
 
 
-def run(offset: int, header: bool = True, mode: str = 'w'):
-    hotels = get_hotels(offset)
-    hotels.to_csv('hotels.csv', encoding='utf-8', index=False, mode=mode, header=header)
-    for id in hotels['id']:
+def run(city_id: int, offset: int, header: bool = True, mode: str = 'w'):
+    restaurants = get_restaurants(city_id, offset)
+    restaurants.to_csv(f'data/{city_id}_restaurants.csv', encoding='utf-8', index=False, mode=mode, header=header)
+    for id in restaurants['id']:
         df = get_reviews(id)
+        
         if df is not None:
-            df.to_csv('hotel_reviews.csv', encoding='utf-8', index=False, mode=mode, header=header)
+            df.to_csv(f'data/{city_id}_restaurant_reviews.csv', encoding='utf-8', index=False, mode=mode, header=header)
 
-    if len(hotels) < 30:
+    if len(restaurants) < 30:
         return False
 
     return True
 
 
 if __name__ == "__main__":
-    get_restaurants(293925, 0)
-    # run(0)
+    city_id = sys.argv[1]
+    run(city_id, 0)
 
-    # offset = 30
-    # try:
-    #     while run(offset, header=False, mode='a'):
-    #         offset += 30
-    # except KeyboardInterrupt:
-    #     print("end!")
+    offset = 30
+    try:
+        while run(city_id, offset, header=False, mode='a'):
+            offset += 30
+    except KeyboardInterrupt:
+        print("end!")
+
